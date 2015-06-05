0d66c1d644e2d3188bb2f3e40b179e6a9408b160
DJu90 <dju90@uw.edu>
2015-06-04 23:07:10 -0700

    now if not in second synack dict, will still print if greater than zero

--- detector.py
+++ detector.py
@@ -30,11 +30,17 @@ def run():
 			if ip.p!=dpkt.ip.IP_PROTO_TCP:
 				continue
 			tcp=ip.data
-			# flags = tcp.flags
+			# fin_flag = ( tcp.flags & dpkt.tcp.TH_FIN ) != 0
+			syn_flag = ( tcp.flags & dpkt.tcp.TH_SYN ) != 0
+			# rst_flag = ( tcp.flags & dpkt.tcp.TH_RST ) != 0
+			# psh_flag = ( tcp.flags & dpkt.tcp.TH_PUSH) != 0
+			ack_flag = ( tcp.flags & dpkt.tcp.TH_ACK ) != 0
+			# urg_flag = ( tcp.flags & dpkt.tcp.TH_URG ) != 0
+			# ece_flag = ( tcp.flags & dpkt.tcp.TH_ECE ) != 0
+			# cwr_flag = ( tcp.flags & dpkt.tcp.TH_CWR ) != 0
 
-			if SYN:
-				if not ACK:
-					print "syn"
+			if syn_flag:
+				if not ack_flag:
 					incr_dict(ip.src, syn_ct)
 				else:
 					incr_dict(ip.dst, syn_ack_ct)				
@@ -44,8 +50,10 @@ def run():
 	print syn_ct
 	print syn_ack_ct
 	for ip in syn_ct:
-		# if ip in syn_ack_ct and syn_ct[ip] > (syn_ack_ct[ip]):
-		print str(ip) + ": " + str(syn_ct[ip]) + ", " + str(syn_ack_ct[ip])
+		if ip in syn_ack_ct and syn_ct[ip] > (syn_ack_ct[ip] * 3):
+			print str(ip) + ": " + str(syn_ct[ip]) + ", " + str(syn_ack_ct[ip])
+		elif ip not in syn_ack_ct and syn_ct[ip] > 0:
+			print str(ip) + ": " + str(syn_ct[ip]) + ", " + str(syn_ack_ct[ip])
 
 def incr_dict(hex_addr, dict):
 	ip_addr = socket.inet_ntoa(str(hex_addr))
